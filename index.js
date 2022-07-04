117ff33b33214d5b0e9a9708a49784a1b76905f6
xinyongjo <xinyongjo@gmail.com>
2022-07-04 13:59:04 +0800

    test git again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = require('./bot.js');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app! ;)');
+  res.send('Hello Express app!');
 });
 
 app.listen(3000, () => {
